c85029e7be83afefc73c59bb8a6a93acba3b3ffe
Quinn Nilsson <quinn.nilsson@gmail.com>
2018-10-16 15:45:53 -0600

    added type module

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+import {films} from '/assets/films.js'
+import {people} from '/assets/people.js'
+import {starships} from 'assets/starships.js'
 
-let openingCrawl = document.querySelector("#openingCrawl")
+let filmList = document.querySelector("#film-list");
+
+films.forEach((film) => {
+    let listItem = document.createElement('li');
+    listItem.textContent = film.opening_crawl;
+    filmList.appendChild(listItem);
+    console.log(film.title);
+})
+    
+
+
+//console.log(films[2]);
+
+/*let openingCrawl = document.querySelector("#openingCrawl")
 fetch('https://swapi.co/api/films/1')
 .then(function(response){
     return response.json();
 })
 .then(function(myJson){
     console.log(myJson.opening_crawl);
-});
+});*/
