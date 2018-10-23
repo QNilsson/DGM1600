0c8202cd180a8c8d22fd2b83b44f1409dd05965d
Quinn Nilsson <quinn.nilsson@gmail.com>
2018-10-23 17:18:25 -0600

    pushing to github, trying to get the api elements to show

--- main.js
+++ main.js
@@ -6,24 +6,29 @@ import {species} from '/assets/species.js'
 import {vehicles} from '/assets/vehicles.js'
 
 //listing some films
-let filmList = document.querySelector("#film-list");
+let listFilms = document.querySelector(".listFilms");
+//let filmListItem = document.createElement('li');
+
 
 films.forEach((film) => {
     let listItem = document.createElement('li');
-    listItem.textContent = film.opening_crawl;
-    filmList.appendChild(listItem);
+    listItem.textContent = film.title;
+    listFilms.appendChild(listItem);
     console.log(film.title);
 })
 
 //listing some people
-let peopleList = document.querySelector("#people-list");
 
 let limitedPeople = people.slice(0,4); //limited people is the array
+let listChars = document.querySelector(".listChars");
+//let charListItem = document.createElement('li');
+
+
     
-    people.forEach((person) => {
+    limitedPeople.forEach((person) => {
     let listItem = document.createElement('li');
     listItem.textContent = person.name;
-    peopleList.appendChild(listItem);    
+    listChars.appendChild(listItem);    
 })
 
 
