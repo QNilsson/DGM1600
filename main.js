ea1e0485052faf38969c76bf2fcc1f2b5693c754
Quinn Nilsson <quinn.nilsson@gmail.com>
2018-10-30 12:45:04 -0600

    adding senators data

--- main.js
+++ main.js
@@ -2,7 +2,30 @@ import { films } from './assets/films.js'
 
 import { people } from './assets/people.js'
 
+import { senatorData } from './assets/senators.js'
 
+
+//senator stuff
+const senators = senatorData.results[0].members
+
+const republicans = senators.filter(senator => senator.party == "R")
+
+const democrats = senators.filter(senator => senator.party == "D")
+
+const females = senators.filter(senator => senator.gender == "F")
+
+const males = senators.filter(senator => senator.gender == "M")
+
+const loyalRepublican = republicans.reduce((acc, senator)=> senator.votes_with_party_pct > 0) ? senator : acc, 0)
+
+
+
+console.log(`The most loyal republican is ${loyalRepublican.first_name} ${loyalRepublican.last_name} 
+who is great and is from ${loyalRepublican.state}`)
+
+
+
+//star wars api stuff
 //listing some films
 let listFilms = document.querySelector(".listFilms");
 //let filmListItem = document.createElement('li');
