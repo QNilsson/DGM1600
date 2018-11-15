8ef480ff6e92cd55629c0107f0aaf7306bd55227
Quinn Nilsson <quinn.nilsson@gmail.com>
2018-11-14 17:33:03 -0700

    adding pokemon code, trying to get correct info on either side of card

--- pokecode.js
+++ pokecode.js
@@ -1,5 +1,7 @@
 import { pokemon } from './pokemon.js'
 
+import { newPoke } from './newPoke.js'
+
 //pokemon stuff/////////////////////////
 
 
@@ -7,77 +9,79 @@ console.log(pokemon)
 
 const pokeContainer = document.querySelector('#container')
 
-pokemon.forEach(poke =>{
-
-    //create a div element called 'card'
+//create cards from the pokemon
+const makeCard = (poke) =>{
+    //create a div element called card
     let card = document.createElement('div')
-    //this is how you will refer to it in css
+    //to refer to in css
     card.className = "card"
-    //add a function to the card
+    //add an event for when card is flipped
     card.addEventListener('click', function(){
-        //when clikced, the card will flip
         card.classList.toggle('is-flipped')
     })
 
-    //make the front side of the card
-    //figure will contain the card image and info
-    let frontSideCard = document.createElement('figure')
-    //refer to it for css
-    frontSideCard.className = "card_face card_face--front"
-    //this info will retrieve the picture
-    console.log(`${poke.id}${poke.ename}.png`)
+//make the front side of the card
+let frontFace = document.createElement('figure')
+frontFace.className = "card_face card_face--front"
+console.log(`${poke.id}${poke.ename}.png`)
 
-    //create a caption for the pokemon image
-    let cap = document.createElement('figcaption')
-    //create an image
-    let img = document.createElement('img')
+let cap = document.createElement('figcaption')
+let img = document.createElement('img')
 
-    //refer to the source of the image using the id and name
-    img.src = `pokeImages/${poke.id}${poke.ename}.png`
-    //make the text content for the caption the pokemon name
-    cap.textContent = poke.ename
-    //use the appendChild so it will actually show up on screen
-    frontSideCard.appendChild(cap)
-    frontSideCard.appendChild(img)
-    //now the caption and image will be visible
+img.src = `pokeImages/${poke.id}${poke.ename}.png`
+cap.textContent = poke.ename
+frontFace.appendChild(cap)
+frontFace.appendChild(img)
 
-    //back side info
-    let backSideCard = document.createElement('figure')
-    //refer to it for css
-    backSideCard.className = "card_face card_face--back"
-    //this will be fore pokemon logo if you want it
-    let capt = document.createElement('figcaption')
-    //let imag = document.createElement('img')
+//back of card
 
-    //for the background pic
-    //imag.src = "images/PokemonLogo.png"
-    //put the pokemon name
-    capt.textContent = poke.ename
-    //make it visible on screen;
-    backSideCard.appendChild(capt)
-    //backSideCard.appendChild(imag)
+let backFace = document.createElement('figure')
+backFace.className = "card_face card_face--back"
+let capt = document.createElement('figcaption')
+let imag = document.createElement('img')
 
-    card.appendChild(frontSideCard)
-    card.appendChild(backSideCard)
-       
+imag.src="pokeImages/Logo-Pokemon.png"
+capt.textContent = poke.ename
 
-})
+let attack = document.createElement('p')
+let defense = document.createElement('p')
+
+attack.textContent = `Attack: ${poke.base.Attack}`
+defense.textContent = `Defense: ${poke.base.Defense}`
+
+backFace.appendChild(imag)
+backFace.appendChild(capt)
+backFace.appendChild(attack)
+backFace.appendChild(defense)
 
 
-//for the user to add his own card
-const newCard = () => {
-    //front of card
-    const makeCard = document.createElement('figure')
-    let caption = document.createElement('figcaption')
-    img.src = document.getElementById("pokeImage")
-    console.log(pokeImage)
-    console.log(pokeName)
-    cap.textContent = document.getElementById("pokeName")
-    makeCard.appendChild(caption)
-    makeCard.appendChild(img)
-    newCard.appendChild(makeCard)
-    pokeContainer.push(newCard)
-
-    //back of card
+card.appendChild(frontFace)
+card.appendChild(backFace)
+pokeContainer.appendChild(card)
 
 }
+
+//user can make new card
+pokemon.forEach(element => makeCard(element))
+
+let newCard = {
+     
+    "ename": "Meowth" ,
+    "id": "052",
+     "base":{
+        "Attack": 70, 
+        "Defense": 60
+
+     } 
+}
+
+let create = document.querySelector('#create')
+create.addEventListener('click', ()=>{
+    return newCard(newCard)
+})
+
+let createCard = document.createElement('div')
+createCard.className = "card"
+createCard.addEventListener('click', ()=>{
+    console.log("gotta catch em all!")
+})
