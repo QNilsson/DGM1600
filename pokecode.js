8313e84ca1ee5210fd0b5e471a5501009180b156
Quinn Nilsson <quinn.nilsson@gmail.com>
2018-11-15 21:16:46 -0700

    idk man, the new card may not be a thing afterall

--- pokecode.js
+++ pokecode.js
@@ -9,82 +9,67 @@ console.log(pokemon)
 
 const pokeContainer = document.querySelector('#container')
 
-
-//create cards from the pokemon
-const cardCreator = (poke =>{
-
-    let card = document.createElement('div')
-    card.className = "card"
-    card.addEventListener('click', function() {
-        card.classList.toggle('is-flipped')
-    })
-
-
+const cardCreator = (poke) =>{
+//create a div for the card
+let card = document.createElement('div')
+card.className = "card"
+//create a figure element for the front side of the card
 let frontCard = document.createElement('figure')
-frontCard.className = ("card_face card_face--front")
-console.log(`${poke.ename}`)
-
+frontCard.className = "card_face card_face--front"
+//this will put each pokemon image on the front side
+let img = document.createElement('img')//individual poke images
+//crate a figure for the back side of the card
+let backCard = document.createElement('figure')
+backCard.className = "card_face card_face--back"
+//make elements for the pokemon name and attributes
 let cap = document.createElement('figcaption')
-let img = document.createElement('img')
+let base = document.createElement('p')
 
+//elements for the poke attributes
+let baseAtk = document.createElement('p')
+let baseDef = document.createElement('p')
+let baseSpeed = document.createElement('p')
+//will make the card flip
+card.addEventListener('click', function(){
+    card.classList.toggle('is-flipped');
+});
+
+//add the text from the pokemon data
+baseAtk.textContent = `${poke.ename} has an attack of (${poke.base["Attack"]})`
+baseDef.textContent = `${poke.ename} has a defense of (${poke.base["Defense"]})`
+baseSpeed.textContent = `${poke.ename} has a speed of (${poke.base["Speed"]})`
+cap.textContent = `${poke.ename}`
 img.src = `pokeImages/${poke.id}${poke.ename}.png`
-cap.textContent = poke.ename
-frontCard.appendChild(cap)
+
+//make visible on screen
 frontCard.appendChild(img)
-
-//card back
-let cardBack = document.createElement('figure')
-cardBack.className = "card_face card_face--back"
-let capt = document.createElement('figcaption')
-let imag = document.createElement('img')
-
-//imag.src = "pokeImages/Logo-Pokemon.png"
-capt.textContent = poke.ename
-
-let attack = document.createElement('p')
-let defense = document.createElement('p')
-let speed = document.createElement('p')
-
-attack.textContent = `Attack: ${poke.attack}`
-defense.textContent = `Defense: ${poke.defense}`
-speed.textContent = `Speed: ${poke.speed}`
-
-cardBack.appendChild(imag)
-cardBack.appendChild(capt)
-cardBack.appendChild(attack)
-cardBack.appendChild(defense)
-cardBack.appendChild(speed)
-
+frontCard.appendChild(cap)
+backCard.appendChild(cap)
+backCard.appendChild(base)
+backCard.appendChild(baseAtk)
+backCard.appendChild(baseDef)
+backCard.appendChild(baseSpeed)
 card.appendChild(frontCard)
-card.appendChild(cardBack)
+card.appendChild(backCard)
 pokeContainer.appendChild(card)
-console.log(poke.ename)
-})
 
+};
 
-//user can create own card
-//needed info for new card
+///////new cards
+pokemon.forEach(element => cardCreator(element))
+
 let newCard = {
     "ename": "Raichu",
     "id": "026",
-    "attack": "50",
-    "defense": "45",
-    "speed": "75"
+    "attack": "45",
+    "defense": "23",
+    "speed": "75"    
 }
 
-//create a clikc event to create the new card
-let create = document.querySelector('#create')
-create.addEventListener('click', () =>{
-    return cardCreator(newCard)
-})
-//give it a class name 
-let createCard = document.createElement('div')
-createCard.className = "card"
-createCard.addEventListener('click', () =>{
-    //make it visible on screen
-    pokeContainer.appendChild(newCard)
-}
-)
 
-//user can make new card//////////////////////////////////////////////////////
-//pokemon.forEach(element => cardCreator(element))
+
+
+
+
+
+
